c2f9c21fb013b75725742f898b22585b3cbd6407
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2023-10-23 08:57:41 +0200

    changes on folder

---
+++ src/components/ui/badge/badge.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { Story } from "@ladle/react";
+import { Badge, type BadgeProps } from '~/components/ui/badge'
+
+export const badge: Story = (props: BadgeProps) => {
+  return <Badge {...props}>Park UI</Badge>
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: "Badge"
+};
